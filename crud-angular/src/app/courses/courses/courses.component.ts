8637767f721988ec78091c0d0c10024b32614268
SINQIA\alesandro.junior <alesandro.junior@sinqia.com.br>
2024-07-16 14:16:43 -0300

    CHECK - Aula06 - Adição da Tabela de Cursos

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class CoursesComponent {
 
+  courses: Course[] = [
+    {_id: "1", name: 'Angular', category: 'Front-End'}
+  ];
+  displayedColumns = ['name', 'category']
+
+  constructor() {
+   // this.courses = [];
+  }
 }
